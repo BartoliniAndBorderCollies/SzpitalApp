2424b5a3f45c51a5ad8fe2e9f66c644b99d8550d
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-12-15 20:15:05 +0100

    dodanie klasy Lekarz

--- ModelSzpitala/Lekarz.cs
+++ ModelSzpitala/Lekarz.cs
@@ -6,7 +6,71 @@ using System.Threading.Tasks;
 
 namespace ModelSzpitala
 {
-    internal class Lekarz
+    internal class Lekarz : Pracownik
     {
+        private Specjalnosc _specjalnosc;
+        private int _numerPWZ;
+        private readonly List<Dyzur> _listaDyzurow = new List<Dyzur>(); //tworze liste raz, a potem mogę ją modyfikować, ale nie nadpisywac
+
+        public Lekarz(string imie, string nazwisko, string pesel, string nazwaUzytkownika, string haslo, Specjalnosc specjalnosc, int numerPWZ, List<Dyzur> listaDyzurow) : base(imie, nazwisko, pesel, nazwaUzytkownika, haslo)
+        {
+            this._specjalnosc = specjalnosc;
+            this._numerPWZ = numerPWZ;
+
+            if (listaDyzurow != null)
+            {
+                _listaDyzurow.AddRange(listaDyzurow);
+            }
+        }
+
+
+        public void DodajDyzur(Dyzur dyzur)
+        {
+
+            if (SprawdzLiczbeDyzurowWMiesiacu(dyzur) >= 10)
+            {
+                Console.WriteLine("Nie można mieć więcej niż 10 dyżurów miesięcznie");
+                return;
+            }
+
+            if (SprawdzDyzurDzienPoDniu(dyzur))
+            {
+                Console.WriteLine("Nie można mieć dyzuru dzień po dniu");
+                return;
+            }
+
+            _listaDyzurow.Add(dyzur);
+        }
+
+        private bool SprawdzDyzurDzienPoDniu(Dyzur dyzur)
+        {
+
+            foreach (Dyzur d in _listaDyzurow)
+            {
+                if (dyzur.DataRozpoczecia.Date == d.DataZakonczenia.Date.AddDays(1) ||
+                    dyzur.DataRozpoczecia.Date == d.DataRozpoczecia.Date.AddDays(-1) ||
+                    dyzur.DataRozpoczecia.Date == d.DataZakonczenia.Date)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private int SprawdzLiczbeDyzurowWMiesiacu(Dyzur dyzur)
+        {
+            int liczbaDyzurowDanyMiesiac = 0;
+            int month = dyzur.DataRozpoczecia.Month;
+            int year = dyzur.DataRozpoczecia.Year;
+
+            foreach (Dyzur d in _listaDyzurow)
+            {
+                if (d.DataZakonczenia.Month == month && d.DataZakonczenia.Year == year)
+                    liczbaDyzurowDanyMiesiac++;
+            }
+
+            return liczbaDyzurowDanyMiesiac;
+        }
     }
 }
