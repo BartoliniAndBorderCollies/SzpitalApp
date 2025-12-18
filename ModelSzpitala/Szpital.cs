ee55da03e5cef8fe4b78c180df1954dbe3b0f342
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-12-18 19:14:35 +0100

    dodanie Singleton - Szpital

---
+++ ModelSzpitala/Szpital.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelSzpitala
+{
+    internal class Szpital
+    {
+        //SINGELTON
+        private static Szpital _szpital;
+
+        private readonly List<Pracownik> _listaPracownikow = new List<Pracownik>();
+
+        public static Szpital SzpitalInstance
+        {
+            get
+            {
+                if (_szpital == null)
+                {
+                    _szpital = new Szpital();
+                }
+                return _szpital;
+            }
+        }
+
+        public IReadOnlyList<Pracownik> DostepDoListyPracownikow
+        {
+            get
+            {
+                return _listaPracownikow;
+            }
+        }
+
+
+        // prywatny konstruktor - nikt poza klasą Szpital nie może wywołać new Szpital()
+        private Szpital()
+        {
+
+        }
+
+        public void DodajPracownika(Pracownik pracownik)
+        {
+            _listaPracownikow.Add(pracownik);
+        }
+
+
+        public void UsunPracownika(String pesel)
+        {
+            Pracownik? pracownikDoUsuniecia = SzukajPracownikaPoPeselu(pesel);
+
+            if (pracownikDoUsuniecia != null)
+            {
+                _listaPracownikow.Remove(pracownikDoUsuniecia);
+            }
+            else
+            {
+                Console.WriteLine("Nie mozna usunac nieistniejacego pracownika");
+                return;
+
+            }
+        }
+
+        public string WyswietlListeWszystkichPracownikow()
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (Pracownik pracownik in DostepDoListyPracownikow)
+            {
+                sb.AppendLine(pracownik.ToString()); //w innym przypadku zwróciłbym tylko pierwszego pracownika: return pracownik.ToString()
+
+            }
+            return sb.ToString();
+        }
+        private Pracownik? SzukajPracownikaPoPeselu(string pesel)
+        {
+
+            foreach (Pracownik pracownik in _listaPracownikow)
+            {
+                if (pracownik.Pesel == pesel)
+                {
+                    return pracownik;
+                }
+            }
+
+            Console.WriteLine("Nie znaleziono pracownika o podanym peselu");
+            return null;
+        }
+
+        public IReadOnlyList<Pracownik> WyswietlListeLekarzyPielegniarek()
+        {
+            List<Pracownik> listaLekarzyPielegniarek = new List<Pracownik>();
+
+            foreach (Pracownik pracownik in _listaPracownikow)
+            {
+                
+                if(pracownik is Lekarz || pracownik is Pielegniarka)
+                {
+                    listaLekarzyPielegniarek.Add(pracownik);
+                }
+            }
+
+            return listaLekarzyPielegniarek;
+
+        }
+    }
+}
