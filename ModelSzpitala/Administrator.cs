c450bb9a0efa27c5044923e5fdce1e12fdcc2c3b
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-12-18 19:14:48 +0100

    dodanie klasy Adminitrator wraz z metodami

---
+++ ModelSzpitala/Administrator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelSzpitala
+{
+    internal class Administrator : Pracownik
+    {
+
+        public Administrator(string imie, string nazwisko, string pesel, string nazwaUzytkownika, string haslo) : base(imie, nazwisko, pesel,nazwaUzytkownika, haslo)
+        {
+        
+        }
+
+        public void DodajNowegoPracownika(Pracownik pracownik)
+        {
+            Szpital.SzpitalInstance.DodajPracownika(pracownik);
+        }
+
+        public string WyswietlListeWszystkichPracownikow()
+        {
+            return Szpital.SzpitalInstance.WyswietlListeWszystkichPracownikow();
+        }
+
+        public void UsunPracownika(String pesel)
+        {
+            Szpital.SzpitalInstance.UsunPracownika(pesel);
+        }
+
+    }
+}
