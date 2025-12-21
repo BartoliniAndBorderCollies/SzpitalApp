5cb9e13af6a2e693f1544338c09fa1de6cbc9741
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-12-21 11:22:15 +0100

    dodanie AddEmployeeForm

---
+++ SzpitalApp/AddEmployeeForm.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace SzpitalApp
+{
+    partial class AddEmployeeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cmbRodzajPracownika = new ComboBox();
+            txtImie = new TextBox();
+            txtNazwisko = new TextBox();
+            txtPesel = new TextBox();
+            txtLogin = new TextBox();
+            txtHaslo = new TextBox();
+            btnZapisz = new Button();
+            btnAnuluj = new Button();
+            label1 = new Label();
+            cmbSpecjalnosc = new ComboBox();
+            SuspendLayout();
+            // 
+            // cmbRodzajPracownika
+            // 
+            cmbRodzajPracownika.FormattingEnabled = true;
+            cmbRodzajPracownika.Items.AddRange(new object[] { "Lekarz", "Pielęgniarka" });
+            cmbRodzajPracownika.Location = new Point(53, 73);
+            cmbRodzajPracownika.Name = "cmbRodzajPracownika";
+            cmbRodzajPracownika.Size = new Size(331, 40);
+            cmbRodzajPracownika.TabIndex = 0;
+            cmbRodzajPracownika.Text = "Wybierz rodzaj pracownika";
+            cmbRodzajPracownika.SelectedIndexChanged += cmbRodzajPracownika_SelectedIndexChanged;
+            // 
+            // txtImie
+            // 
+            txtImie.Location = new Point(53, 134);
+            txtImie.Name = "txtImie";
+            txtImie.PlaceholderText = "Imię";
+            txtImie.Size = new Size(200, 39);
+            txtImie.TabIndex = 1;
+            // 
+            // txtNazwisko
+            // 
+            txtNazwisko.Location = new Point(53, 196);
+            txtNazwisko.Name = "txtNazwisko";
+            txtNazwisko.PlaceholderText = "Nazwisko";
+            txtNazwisko.Size = new Size(200, 39);
+            txtNazwisko.TabIndex = 2;
+            // 
+            // txtPesel
+            // 
+            txtPesel.Location = new Point(513, 74);
+            txtPesel.Name = "txtPesel";
+            txtPesel.PlaceholderText = "Pesel";
+            txtPesel.Size = new Size(200, 39);
+            txtPesel.TabIndex = 3;
+            // 
+            // txtLogin
+            // 
+            txtLogin.Location = new Point(513, 134);
+            txtLogin.Name = "txtLogin";
+            txtLogin.PlaceholderText = "Login";
+            txtLogin.Size = new Size(200, 39);
+            txtLogin.TabIndex = 4;
+            // 
+            // txtHaslo
+            // 
+            txtHaslo.Location = new Point(513, 196);
+            txtHaslo.Name = "txtHaslo";
+            txtHaslo.PlaceholderText = "Hasło";
+            txtHaslo.Size = new Size(200, 39);
+            txtHaslo.TabIndex = 5;
+            txtHaslo.UseSystemPasswordChar = true;
+            txtHaslo.TextChanged += txtHaslo_TextChanged;
+            // 
+            // btnZapisz
+            // 
+            btnZapisz.BackColor = SystemColors.GradientActiveCaption;
+            btnZapisz.Location = new Point(103, 371);
+            btnZapisz.Name = "btnZapisz";
+            btnZapisz.Size = new Size(150, 46);
+            btnZapisz.TabIndex = 6;
+            btnZapisz.Text = "Zapisz";
+            btnZapisz.UseVisualStyleBackColor = false;
+            btnZapisz.Click += btnZapisz_Click;
+            // 
+            // btnAnuluj
+            // 
+            btnAnuluj.Location = new Point(513, 371);
+            btnAnuluj.Name = "btnAnuluj";
+            btnAnuluj.Size = new Size(150, 46);
+            btnAnuluj.TabIndex = 7;
+            btnAnuluj.Text = "Anuluj";
+            btnAnuluj.UseVisualStyleBackColor = true;
+            btnAnuluj.Click += btnAnuluj_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(273, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(260, 32);
+            label1.TabIndex = 8;
+            label1.Text = "Dodawanie pracownika";
+            // 
+            // cmbSpecjalnosc
+            // 
+            cmbSpecjalnosc.FormattingEnabled = true;
+            cmbSpecjalnosc.Items.AddRange(new object[] { "Kardiolog", "Neurolog", "Laryngolog" });
+            cmbSpecjalnosc.Location = new Point(53, 259);
+            cmbSpecjalnosc.Name = "cmbSpecjalnosc";
+            cmbSpecjalnosc.Size = new Size(242, 40);
+            cmbSpecjalnosc.TabIndex = 9;
+            cmbSpecjalnosc.Text = "Specjalność";
+            cmbSpecjalnosc.Visible = false;
+            // 
+            // AddEmployeeForm
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(cmbSpecjalnosc);
+            Controls.Add(label1);
+            Controls.Add(btnAnuluj);
+            Controls.Add(btnZapisz);
+            Controls.Add(txtHaslo);
+            Controls.Add(txtLogin);
+            Controls.Add(txtPesel);
+            Controls.Add(txtNazwisko);
+            Controls.Add(txtImie);
+            Controls.Add(cmbRodzajPracownika);
+            Name = "AddEmployeeForm";
+            Text = "AddEmployeeForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox cmbRodzajPracownika;
+        private TextBox txtImie;
+        private TextBox txtNazwisko;
+        private TextBox txtPesel;
+        private TextBox txtLogin;
+        private TextBox txtHaslo;
+        private Button btnZapisz;
+        private Button btnAnuluj;
+        private Label label1;
+        private ComboBox cmbSpecjalnosc;
+
+        private void txtHaslo_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+
+
+
+    }
---
+++ SzpitalApp/AddEmployeeForm.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualBasic.Logging;
+using ModelSzpitala;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace SzpitalApp
+{
+    public partial class AddEmployeeForm : Form
+    {
+        public AddEmployeeForm()
+        {
+            InitializeComponent();
+
+            cmbSpecjalnosc.DataSource = Enum.GetValues(typeof(Specjalnosc));
+            cmbSpecjalnosc.Hide();
+        }
+
+        private void cmbRodzajPracownika_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string? wybranyRodzaj = cmbRodzajPracownika.SelectedItem?.ToString();
+
+            if (wybranyRodzaj == "Lekarz")
+            {
+                cmbSpecjalnosc.Show();
+            }
+            else
+            {
+                cmbSpecjalnosc.Hide();
+            }
+        }
+
+        private void btnAnuluj_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnZapisz_Click(object sender, EventArgs e)
+        {
+            string? rodzajPracownika = cmbRodzajPracownika.SelectedItem?.ToString();
+            string imie = txtImie.Text;
+            string nazwisko = txtNazwisko.Text;
+            string pesel = txtPesel.Text;
+            string login = txtLogin.Text;
+            string haslo = txtHaslo.Text;
+            Specjalnosc specjalnosc = (Specjalnosc)cmbSpecjalnosc.SelectedItem;
+
+            if (CzyPodanoWszystkieDane(rodzajPracownika, imie, nazwisko, pesel, login, haslo))
+            {
+                DodajPracownika(rodzajPracownika, imie, nazwisko, pesel, login, haslo, specjalnosc);
+
+            }
+            else
+            {
+                return;
+            }
+            this.Close();
+        }
+
+        private void DodajPracownika(string rodzajPracownika, string imie, string nazwisko, string pesel, string login, string haslo, Specjalnosc specjalnosc)
+        {
+            if (rodzajPracownika == "Lekarz")
+            {
+                Lekarz lekarz = new Lekarz(imie, nazwisko, pesel, login, haslo, specjalnosc, 0, new List<Dyzur>());
+                ModelSzpitala.Szpital.SzpitalInstance.DodajPracownika(lekarz);
+                MessageBox.Show("Utworzono pracownika: lekarz");
+            }
+
+            if (rodzajPracownika == "Pielęgniarka")
+            {
+                Pielegniarka pielegniarka = new Pielegniarka(imie, nazwisko, pesel, login, haslo, new List<Dyzur>());
+                ModelSzpitala.Szpital.SzpitalInstance.DodajPracownika(pielegniarka);
+                MessageBox.Show("Utworzono pracownika: pielęgniarka");
+            }
+
+        }
+
+        private bool CzyPodanoWszystkieDane(string rodzajPracownika, string imie, string nazwisko, string pesel, string login, string haslo)
+        {
+            if (string.IsNullOrWhiteSpace(rodzajPracownika) ||
+                string.IsNullOrWhiteSpace(imie) ||
+                string.IsNullOrWhiteSpace(nazwisko) ||
+                string.IsNullOrWhiteSpace(pesel) ||
+                string.IsNullOrWhiteSpace(login) ||
+                string.IsNullOrWhiteSpace(haslo))
+            {
+                MessageBox.Show("Uzupełnij wszystkie pola!");
+                return false;
+            }
+
+            if (rodzajPracownika == "Lekarz" && cmbSpecjalnosc.SelectedItem == null)
+            {
+                MessageBox.Show("Wybierz specjalność lekarza!");
+                return false;
+            }
+
+            return true;
+
+        }
+
+    }
+}
