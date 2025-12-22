eb875df25b72cf1bc47ef30ae247517eb36de60f
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-12-22 13:22:13 +0100

    ustawienie wielkosci bazowej w BaseForm

--- SzpitalApp/BaseForm.cs
+++ SzpitalApp/BaseForm.cs
@@ -15,17 +15,30 @@ namespace SzpitalApp
         public BaseForm()
         {
             this.StartPosition = FormStartPosition.CenterScreen;
-            UstawMaxRozmiar();
         }
 
-        private void UstawMaxRozmiar()
+        protected override void OnShown(EventArgs e)
         {
+            base.OnShown(e);
+
             var ekran = Screen.PrimaryScreen.WorkingArea;
 
-            this.MaximumSize = new Size(
-                (int)(ekran.Width * 0.6),
-                (int)(ekran.Height * 0.6)
+            this.Size = new Size(
+                (int)(ekran.Width * 0.8),
+                (int)(ekran.Height * 0.8)
             );
+
+            this.MinimumSize = new Size(800, 600);
+            this.MaximumSize = new Size(
+                (int)(ekran.Width * 0.8),
+                (int)(ekran.Height * 0.8)
+            );
+
+            this.Location = new Point(
+            (ekran.Width - this.Width) / 2,
+            (ekran.Height - this.Height) / 2
+);
         }
+
     }
 }
