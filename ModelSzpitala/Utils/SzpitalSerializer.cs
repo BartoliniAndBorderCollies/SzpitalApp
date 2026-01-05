790d8c3689fff5461063bf0fcb71d4d5ce42c531
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2026-01-05 17:22:13 +0100

    dodanie klasy serializującej i deserializującej wraz z metodami

---
+++ ModelSzpitala/Utils/SzpitalSerializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ModelSzpitala.Utils
+{
+    public static class SzpitalSerializer
+    {
+
+        private static readonly string FilePath = "szpital.json";
+
+        public static void Zapisz(Szpital szpital)
+        {
+
+            var niceFormat = new JsonSerializerOptions
+            {
+                WriteIndented = true, //nadaje ładny format JSON-owi
+            };
+
+            string json = JsonSerializer.Serialize(szpital, niceFormat);
+            File.WriteAllText(FilePath, json);
+        }
+
+
+        public static Szpital? Wczytaj()
+        {
+            if (!File.Exists(FilePath))
+              return null;
+
+            string json = File.ReadAllText(FilePath);
+            return JsonSerializer.Deserialize<Szpital>(json);
+            
+        }
+    }
+}
