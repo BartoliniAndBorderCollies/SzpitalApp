079879c6bda89cabfa8a2425e208b0ddf6d2a63a
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2026-01-22 12:14:04 +0100

    dodanie klasy LogowanieZdarzenie

---
+++ ModelSzpitala/Security/LogowanieZdarzenie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelSzpitala.Security
+{
+    public class LogowanieZdarzenie
+    {
+        private DateTime _dataLogowania;
+        private string _nazwaUsera;
+        private bool _sukces;
+
+        public DateTime PobierzDate
+        {
+            get
+            {
+                return _dataLogowania;
+            }
+        }
+
+        public string PobierzNazweUsera
+        {
+            get
+            {
+                return _nazwaUsera;
+            }
+        }
+
+        public bool Sukces
+        {
+            get
+            {
+                return _sukces;
+            }
+        }
+    }
+}
