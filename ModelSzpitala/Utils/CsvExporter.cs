8ec4a4053c8df366f0d46f3a9408678fb18d3690
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2026-01-31 12:16:27 +0100

    dodanie eksportu po stronie logiki biznesowej

--- ModelSzpitala/Utils/CsvExporter.cs
+++ ModelSzpitala/Utils/CsvExporter.cs
@@ -1,6 +1,7 @@
 ﻿using ModelSzpitala.Security;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,21 @@ namespace ModelSzpitala.Utils
             foreach (ZdarzenieLogowania logowanie in ListaLogowan)
             {
                 string linia = $"{logowanie.DataLogowania};" + $"{logowanie.NazwaUsera};" + $"{logowanie.Sukces}";
+                stringBuilder.AppendLine(linia);
+
+            }
+
+            File.WriteAllText(sciezkaDoZapisu, stringBuilder.ToString(), Encoding.UTF8);
+        }
+
+        public static void EksportujListeDyzurowDanejOsoby(IReadOnlyList<Dyzur> listaDyzurow, string sciezkaDoZapisu, Pracownik pracownik)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("Pracownik; Data_rozpoczecia; Data_zakonczenia");
+
+            foreach (Dyzur dyzur in listaDyzurow)
+            {
+                string linia = $"{pracownik.NazwaUzytkownika};" + $"{dyzur.DataRozpoczecia};" + $"{dyzur.DataZakonczenia}";
                 stringBuilder.AppendLine(linia);
 
             }
