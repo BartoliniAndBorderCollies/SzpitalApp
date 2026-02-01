66c43c7f2b626b232bc1049871817fc63dcf288d
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2026-02-01 13:30:14 +0100

    dodanie enuma Akcja - potrzebny do Audit log

---
+++ ModelSzpitala/Akcja.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelSzpitala
+{
+    internal enum Akcja
+    {
+        Logowanie,
+
+        Wylogowanie,
+
+        DodaniePracownika,
+
+        EdytujPracownika,
+
+        UsunieciePracownika,
+
+        WyswietlenieListePracownikow,
+
+        WyswietlenieDyzurow,
+
+        DodanieDyzuru,
+
+        UsuniecieDyzuru,
+
+        EksportDyzurow,
+
+        WyswietlenieHistoriiLogowan,
+
+        EkportHistoriiLogowan,
+
+        ZamkniecieProgramu
+
+    }
+
+}
