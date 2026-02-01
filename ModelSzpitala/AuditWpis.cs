fb45f661cb519ff4386d7597a31998f625f3d303
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2026-02-01 13:31:15 +0100

    dodanie modelu domenowego Audit log

---
+++ ModelSzpitala/AuditWpis.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelSzpitala
+{
+    internal class AuditWpis
+    {
+
+        private int _userId;
+        private Akcja _rodzajAkcji;
+        private DateTime _data;
+
+        public AuditWpis(int userId, Akcja rodzajAkcji, DateTime data)
+        {
+            _userId = userId;
+            _rodzajAkcji = rodzajAkcji;
+            _data = data;
+
+        }
+
+        public int DostepDoUserId
+        {
+            get
+            {
+                return _userId;
+            }
+        }
+
+        public Akcja DostepDoRodzajAkcji
+        {
+            get
+            {
+                return _rodzajAkcji;
+            }
+        }
+
+        public DateTime Data
+        {
+            get
+            {
+                return _data;
+            }
+        }
+    }
+}
